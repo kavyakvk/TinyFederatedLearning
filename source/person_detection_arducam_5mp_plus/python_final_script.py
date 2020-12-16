3dbfca743f5c907784e9bc83cb59421188813239
Eric Lin <eric_lin@college.harvard.edu>
2020-12-16 14:27:08 -0500

    New tf weights

--- source/person_detection_arducam_5mp_plus/python_final_script.py
+++ source/person_detection_arducam_5mp_plus/python_final_script.py
@@ -52,18 +52,20 @@ def main():
 	# print(masked_batched[0])
 
 	# Initial Weights
-	init_weights = pickle.load(open('../../dl/pickle_initial_model_weights.p', 'rb'))
-	init_bias = [0.08060145, -0.08060154];			# bias (initial)
-	init_weights.extend(init_bias)					# extend bias to end of 1d weights array
-	init_weights_str = (",".join(map(str, init_weights))[:1000] + "\n").encode()
-	print(init_weights_str)		
-	# print(len(init_weights))		# 256*2 + 2
-	# print(len(init_weights_str))
+    init_weights = pickle.load(open('../../dl/pickle_initial_model_weights.p', 'rb'))
+    init_bias = [0.08060145, -0.08060154];          # bias for tf (initial)
+    init_weights.extend(init_bias)                  # extend bias to end of 1d weights array
+    init_weights_str = (",".join(map(str, init_weights)) + "|").encode()
+    print(init_weights_str)
+    len_weights_str = len(init_weights_str)
+    weights_iters = len_weights_str // 255
+    print(len_weights_str)
+    print(len(init_weights))
 
 
 	# Setup connection to Arduino
 	# port = '/dev/cu.usbmodem142301' # change this to what the Arduino Port is
-	# ard = serial.Serial(port,9600,timeout=5)
+	# ard = serial.Serial(port,9600,timeout=10)
 	time.sleep(3) # wait for Arduino
 
 	# For one round
@@ -96,17 +98,38 @@ def main():
 		# msg = ard.read(ard.inWaiting()) # read all characters in buffer
 		# print(f"Mac: received {msg}")
 
-		# Send initial weights
-		ard.write(init_weights_str)
-		# ard.write("0.63,0.68,0.12,0.12,0.12,-0.1\n".encode())
-		print("Mac: sent initial weights and bias")
-		time.sleep(10)
+		# _________SENDING WEIGHTS___________
+		for j in range(weights_iters):
+            # Serial write section
+            ard.flush()
+            # print("Mac: sent weights and bias")
+            ard.write(init_weights_str[j*255:(j+1)*255])
 
-		for i in range(5):
-			# Read Arduino's response
-			msg = ard.read(ard.inWaiting()) # read all characters in buffer
-			print(f"Mac: received {msg}")
-			time.sleep(10)
+            time.sleep(1)
+
+            # Read Arduino's response
+            msg = ard.read(ard.inWaiting()) # read all characters in buffer
+            print(f"Arduino received {msg.decode('utf-8')} out of {len_weights_str} bytes")
+
+        # Send last remaining chars
+        if weights_iters * 255 < len_weights_str:
+            print(f"last remaining: {init_weights_str[weights_iters*255:]}")
+            ard.write(init_weights_str[weights_iters*255:])
+
+        # ________GETTING WEIGHTS_________
+        output_weights_str = ''
+        num_packets = 8
+        for j in range(num_packets):
+            time.sleep(5)
+            # Read Arduino's response
+            msg = ard.read(ard.inWaiting()) # read all characters in buffer
+            print(f"Server: receiving weight packet {j} out of {num_packets}")
+            output_weights_str += msg.decode("utf-8")
+            # print(f"Mac: received {output_weights_str}")
+        # print(output_weights_str)
+        output_weight_lst = list(map(float, output_weights_str.split()))
+        print(output_weight_lst)
+        print(len(output_weight_lst))
 
 
 		i = i + 1
